0f8b62de471431adf8a8f88469c2125eb9125fa1
webishan <tkhanishan@gmail.com>
2026-04-12 09:35:43 +0600

    datatypes added

---
+++ 02.JavaScript/02.JavaScript Data Types and Operators/datatype.js
@@ -0,0 +1,47 @@
+// let num = -10.25;
+
+// console.log(typeof num);
+
+// let fullName = "Ishan Khan";
+
+// console.log(typeof typeof num);
+
+str = "Hello World";
+
+// console.log(str.toUpperCase());
+
+// console.log(str.toLowerCase());
+
+// let und = undefined;
+// let none = null;
+// console.log(typeof none);
+
+
+// let arr = {a:1};
+// console.log(typeof arr);
+
+// let num1 = 10;
+// let num2 = 5;
+
+// console.log(num1 - num2);
+
+// console.log(5**2)
+
+
+// let num = 10;
+
+// let newNum = num++;
+
+// console.log(newNum);
+
+
+// console.log(3 === '3');
+
+
+// console.log(5 != 4);
+
+
+let age = 27;
+let addr = 'Kamarkhali';
+
+console.log(age > 18 && addr === 'Kamarkhali');
